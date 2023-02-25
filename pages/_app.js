960d3a617c70dbb55c146f4d500fb21d8a81a90a
Glowit Labs <glowitsofware@gmail.com>
2023-02-25 09:05:43 +0530

    vercel web vitals integrated

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
 import '../styles/globals.css'
 
+export function reportWebVitals(metric) {
+  switch (metric.name) {
+    case 'FCP':
+      // handle FCP results
+      break;
+    case 'LCP':
+      // handle LCP results
+      break;
+    case 'CLS':
+      // handle CLS results
+      break;
+    case 'FID':
+      // handle FID results
+      break;
+    case 'TTFB':
+      // handle TTFB results
+      break;
+    case 'INP':
+      // handle INP results (note: INP is still an experimental metric)
+      break;
+    default:
+      break;
+  }
+}
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
